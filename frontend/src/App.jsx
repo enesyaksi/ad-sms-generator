b77ca4ab67cf40b1e7d1d4c327fec3e9fa895132
Enes Yakşi <enesyaksi@gmail.com>
2026-01-29 12:17:42 +0300

    feat(frontend): update App routing to include Overview page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
+import Overview from './pages/Overview';
 import SavedCampaigns from './pages/SavedCampaigns';
 import Settings from './pages/Settings';
 import Login from './pages/Login';
@@ -22,9 +23,10 @@ function App() {
           {/* Protected Routes */}
           <Route element={<ProtectedRoute />}>
             <Route path="/" element={<Layout />}>
-              <Route index element={<Dashboard />} />
+              <Route index element={<Overview />} />
+              <Route path="customers" element={<Dashboard />} />
+              <Route path="campaigns" element={<SavedCampaigns />} />
               <Route path="generator" element={<Home />} />
-              <Route path="saved" element={<SavedCampaigns />} />
               <Route path="settings" element={<Settings />} />
             </Route>
           </Route>
